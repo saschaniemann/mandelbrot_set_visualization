cd560cd4bb5638215f64177bc7cb4008a5909459
sniemann <sascha.niemann66@gmx.de>
2024-07-25 10:43:50 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    //     fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+    //     return 1;
+    // }
+    std::cout << "drin" << std::endl;
+}
